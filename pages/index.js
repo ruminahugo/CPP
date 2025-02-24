c9d45677885cbbc0bc1a22a0661c53246a330a41
ruminahugo <tinhyeuthuhanvasutradua@gmail.com>
2025-02-24 05:00:38 +0000

    Fix lỗi commit trong Codespaces

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,7 @@ export default function GetPassPage() {
     return res.json();
   })
   .then(data => {
+    setNotifi("");
     setData(data.result);
     navigator.clipboard.writeText(data.result); // Copy vào clipboard
   })
