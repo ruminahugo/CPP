ed7605fe148d9b1f100940a9f0923db87636f502
ruminahugo <tinhyeuthuhanvasutradua@gmail.com>
2025-02-25 05:53:17 +0000

    Fix lỗi commit trong Codespaces

--- middleware.js
+++ middleware.js
@@ -2,11 +2,15 @@ import { NextResponse } from 'next/server';
 
 const allowedIps = ['203.171.27.84']; // Danh sách IP được phép
 
-export function middleware(req, res) {
+export function middleware(req) {
     const clientIp = req.ip || req.headers.get("x-forwarded-for") || req.headers.get("cf-connecting-ip");
 
     if (!clientIp || !allowedIps.includes(clientIp)) {
         return new NextResponse('Access Denied', { status: 403 });
+    }
+
+    if (req.nextUrl.protocol === "http:") {
+        return NextResponse.redirect(`https://${req.nextUrl.host}${req.nextUrl.pathname}`, 301);
     }
 
     return NextResponse.next();
