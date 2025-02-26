00ae68b400e0c79d0df557b21ef946586d1a6643
ruminahugo <tinhyeuthuhanvasutradua@gmail.com>
2025-02-26 01:10:08 +0000

    Fix lỗi commit trong Codespaces

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
         <title>Vinschool - Get Password Random</title>
         <meta name="description" content="Tool nội bộ random password cho hệ thống Vinschool." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="../public/img/favicon.ico" />
+        <link rel="icon" href="/img/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
