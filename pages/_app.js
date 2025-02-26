ed840e77b04a2db35d215c5da7a3bf9787c308b7
ruminahugo <tinhyeuthuhanvasutradua@gmail.com>
2025-02-26 00:49:32 +0000

    Fix lỗi commit trong Codespaces

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+//import '@/styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Mặc định - Website của tôi</title>
+        <meta name="description" content="Tool nội bộ random password cho hệ thống Vinschool." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="../public/img/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
